5c98fc88016c8968117acf685c20fffe69619e13
SOFTSERVE\sfili <sfili@softserveinc.com>
2018-04-02 11:59:44 +0300

    create login form and auth hoc

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,13 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Layout from './hoc/Layout';
 import PostView from './components/Posts';
- 
+import Login from './containers/Admin/login';
+import Auth from './hoc/auth';
+import User from './components/Admin';
+
 const Routes = () => {
     return (
         <Layout>
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/allNews/:id" component={PostView} />
+                <Route path="/" exact component={Auth(Home, null)} />
+                <Route path="/login" exact component={Auth(Login, false)} />
+                <Route path="/user" exact component={Auth(User, true)} />
+                <Route path="/allNews/:id" component={Auth(PostView)} />
             </Switch>
         </Layout>
     );
